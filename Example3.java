3112a8e5da7aea2873d5b5da932bade10d99e878
kurt <kurtlemond@gmail.com>
2014-04-09 10:53:29 +0800

    Added var3 on example3.java

--- Example3.java
+++ Example3.java
@@ -1,10 +1,11 @@
 class Example3{
 	public static void main(String args[]){
 	int var;
-	float var2;
+	float var2,var3;
 	
 	var = 20;
 	var2 = 20;
+	var3 = 40;
 	
 	System.out.println("variable var is equal to " + var);
 	System.out.println("while var2 is equal to " + var2);
@@ -13,5 +14,6 @@ class Example3{
 	var2 = var2/9;
 	System.out.println("the one-fourth of var is " + var);
 	System.out.println("while the one-fourth of var2 is " + var2);
+	System.out.println("Epal tong var3");
 	}
 	}
